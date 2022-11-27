7a235232e0890040a3e3531400f49778163fdc8c
xyznazmul <119147442+nazmuljod@users.noreply.github.com>
2022-11-27 11:23:02 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,33 @@ const searchBarIsActive = () => navbarForm.classList.toggle('active');
 
 navbarSearchBtn.addEventListener('click', searchBarIsActive);
 navbarFormCloseBtn.addEventListener('click', searchBarIsActive);
+
+$(".Click-here").on('click', function() {
+  $(".custom-model-main").addClass('model-open');
+}); 
+$(".close-btn, .bg-overlay").click(function(){
+  $(".custom-model-main").removeClass('model-open');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TAB
+// $(document).on('click', '.tab-wrap ul li a', function (e) {
+//     var curTabContentId = $(this).attr('href');
+//     $(this).parents('.tab-wrap').find('ul li').removeClass('active');
+//     $(this).parents('li').addClass('active');
+//     $(this).parents('.tab-wrap').find('.tab-content-id').removeClass('active');
+//     $(curTabContentId).addClass("active");
+//     e.preventDefault();
+// });
